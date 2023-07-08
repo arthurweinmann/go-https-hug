d878bf797011dee4a248b11e28ca129b3b53c533
Arthur Weinmann <arthur.weinmann@gmail.com>
2023-07-08 00:31:27 -0500

    move example function into test file

--- pkg/acme/get.go
+++ pkg/acme/get.go
@@ -39,39 +39,6 @@ func GetCertificate(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
 	return tlscert, nil
 }
 
-func ExampleGetCertificate() {
-	conn, err := net.Listen("tcp", ":443")
-	if err != nil {
-		log.Fatal(err)
-	}
-	tlsConfig := new(tls.Config)
-	tlsConfig.GetCertificate = GetCertificate
-	tlsListener := tls.NewListener(conn, tlsConfig)
-
-	f, err := os.OpenFile("https.log", os.O_CREATE|os.O_RDWR, 0700)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	serv := &http.Server{
-		Addr:     ":443",
-		Handler:  http.NewServeMux(),
-		ErrorLog: log.New(f, "https: ", log.Llongfile|log.Ltime|log.Ldate),
-
-		ReadHeaderTimeout: 30 * time.Second,
-		ReadTimeout:       1 * time.Minute,
-		WriteTimeout:      1 * time.Minute,
-		IdleTimeout:       5 * time.Minute,
-	}
-
-	fmt.Println("Starting HTTPS Server")
-	serv.Serve(tlsListener)
-
-	// Output:
-	// Starting HTTPS Server
-}
-
 // Serve is blocking
 // Example of addr is :443
 // logfilepath is optional and can be empty
